28519f220e92718e22913cf588db5db25767ac86
unknown <marwahadriani8@gmail.com>
2024-03-02 22:30:47 +0700

    latihanlooping

---
+++ angka.java
@@ -0,0 +1,13 @@
+public class angka {
+    
+    public static void main(String[] argume){
+
+        for(int i=0; i <= 11; i++){
+            System.out.print( i + " " );
+        }
+
+    }
+    }
+
+
+
